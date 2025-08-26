f38406e0225b74119ae8704ab1823fc4aae692cb
Zion Omogbeme <omogbemezion@gmail.com>
2025-08-26 13:53:37 +0100

    added new classes

---
+++ Emplyees.py
@@ -0,0 +1,9 @@
+class Employees():
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+
+    def show_details(self):
+        print(f"Employee Name: {self.name}")
+        print(f"Salary: {self.salary}")
