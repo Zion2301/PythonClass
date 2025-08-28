78f18ed33915a21deccddb82bb32ddc408043203
Zion Omogbeme <omogbemezion@gmail.com>
2025-08-28 13:12:18 +0100

    adding inversion

--- matrix.py
+++ matrix.py
@@ -2,14 +2,21 @@ import numpy as np
 
 
 #addition of a matrix in python
-A = np.array([[1, 2], [3, 4]])
+A = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 B = np.array([[5, 6], [7, 8]])
 
-print(A+B)
+# print(A+B)
 
 
 #multiplication of a scalar matrix
 # print(2*A)
 
-#to transpose a matrix
+#to transpose a matrix(hcnaging rows to columns)
 # print(A.T)
+
+
+#determinant
+# print(np.linalg.det(B))
+
+#inverse a matrix
+print(np.linalg.inv(B))
