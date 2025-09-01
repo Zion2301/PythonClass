1462feedf6bd3ca396981258ca075686eb8c72c2
Zion Omogbeme <omogbemezion@gmail.com>
2025-09-01 12:43:06 +0100

    printed a 3d array

--- nump.py
+++ nump.py
@@ -9,5 +9,9 @@ print(type(arr))
 # arr = np.array(45)
 # print(arr)
 
-arr = np.array([[1, 2, 3], [4, 5, 6]])
-print(arr)
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+# print(arr)
+
+#now 3d array
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr.ndim)     #to get the dimesnions an array is in
