51f3fe8eccd95e74520006c8445aba87022ca3fe
Zion Omogbeme <omogbemezion@gmail.com>
2025-09-01 12:55:16 +0100

    getting numbers from a 3d array

--- nump.py
+++ nump.py
@@ -3,7 +3,7 @@ arr = np.array([1, 2, 3, 4, 5])
 # print(arr)
 
 #printing the type
-print(type(arr))
+# print(type(arr))
 
 #now for 2d array
 # arr = np.array(45)
@@ -13,5 +13,12 @@ print(type(arr))
 # print(arr)
 
 #now 3d array
-arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(arr.ndim)     #to get the dimesnions an array is in
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr.ndim)     #to get the dimesnions an array is in
+
+# arr = np.array([10, 20, 30, 40, 50])
+# # print(arr[0])           #getting an index in the array
+# print(arr.sum())        #summing an array
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [30, 40, 50]])
+print(arr[2, 1])        #accessing values from a 3d array, just multidimensional in general
