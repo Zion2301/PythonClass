a0fef6dfcc571c0542d40dfd5278191150296314
Zion Omogbeme <omogbemezion@gmail.com>
2025-09-01 12:40:59 +0100

    printed 2d aray

--- nump.py
+++ nump.py
@@ -1,6 +1,13 @@
-import numpy
-arr = numpy.array([1, 2, 3, 4, 5])
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
 # print(arr)
 
 #printing the type
 print(type(arr))
+
+#now for 2d array
+# arr = np.array(45)
+# print(arr)
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr)
